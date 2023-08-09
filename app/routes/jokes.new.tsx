7e6144acf51af5e3a3a6ced81d496bd161623767
Yoshimasa Suzuki <cognac0004@gmail.com>
2023-08-09 15:50:16 +0900

    after install prisma

---
+++ app/routes/jokes.new.tsx
@@ -0,0 +1,32 @@
+// import type { LinksFunction } from "@remix-run/node";
+
+// import stylesUrl from "~/styles/index.css";
+
+// export const links: LinksFunction = () => [
+//   { rel: "stylesheet", href: stylesUrl },
+// ];
+
+export default function NewJokeRoute() {
+  return (
+    <div>
+      <p>Add your own hilarious joke</p>
+      <form method="post">
+        <div>
+          <label>
+            Name: <input type="text" name="name" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Content: <textarea name="content" />
+          </label>
+        </div>
+        <div>
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
---
+++ app/routes/jokes.tsx
@@ -0,0 +1,19 @@
+import type { LinksFunction } from "@remix-run/node";
+import { Outlet } from "@remix-run/react";
+
+import stylesUrl from "~/styles/jokes.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
+export default function JokesRoute() {
+  return (
+    <div>
+      <h1>J🤪KES</h1>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
